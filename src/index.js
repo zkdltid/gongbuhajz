c6d4d15d2b6c71b6a728e3587f6b5b5980e67088
KaiChan <zkdltid28@gmail.com>
2020-11-27 17:24:16 +0800

    增加切頁功能

--- src/index.js
+++ src/index.js
@@ -5,139 +5,114 @@ import './css/table.css';
 import { fetchTopik2Vocabulary } from './js/api/context/topik';
 import reportWebVitals from './reportWebVitals';
 
-class TestOne extends React.Component {
+class Topik2Vocabulary extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      total:null,
       page_index: 1,
-      size: 15,
+      size: 10,
       data_list: [],
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleNextButon = this.handleNextButon.bind(this);
+    this.handlePrevButon = this.handlePrevButon.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ size: event.target.value });
+    event.preventDefault();
+    this.setState({ size: event.target.value,
+      page_index: 1,
+     });
+    this.callApi({ 
+      size: event.target.value,
+     });
+  }
+  
+  handleNextButon() {
+    let new_page_index = this.state.page_index+1
+    this.setState({ page_index: new_page_index });
+    this.callApi({ 
+      page_index: new_page_index,
+      size: this.state.size,
+    } );
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    this.callApi();
-    }
+  handlePrevButon() {
+    let new_page_index = this.state.page_index-1
+    this.setState({ page_index: new_page_index });
+    this.callApi({ 
+      page_index: new_page_index,
+      size: this.state.size,
+    } );
+  }  
 
-    callApi = () => {
-      fetchTopik2Vocabulary({ size:this.state.size })
+  callApi = (params) => {
+    fetchTopik2Vocabulary(params)
       .then(response => {
         console.log(response);
         this.setState({ data_list: response.data.data_list });
+        this.setState({ total: response.data.total });
         console.log(this.state.data_list);
       })
       .catch(error => {
         console.log(error);
       });
-    }
-    
+  }
 
-    componentDidMount(){
-      this.callApi();
-    }
+
+  componentDidMount(){
+    this.callApi();
+  }
+
   render() {
     return (
       <div className='table'>
-      <form onSubmit={this.handleSubmit}>
-      <select value={this.state.size} onChange={this.handleChange}>
-        <option value='10'>10</option>
-        <option value="20">20</option>
-        <option value="30">30</option>
-      </select>
-      <br />
-      <input type="submit" value="送出" />
-    </form>
+        <div>
+            <select value={this.state.size} onChange={this.handleChange}>
+              <option value='10'>10</option>
+              <option value="20">20</option>
+              <option value="30">30</option>
+              <option value="40">40</option>
+              <option value="50">50</option>
+            </select>
+          <button onClick={this.handlePrevButon}>
+            Prev Page
+          </button>
+          <button onClick={this.handleNextButon}>
+            Next Page
+          </button>
+          <div>Page Index : {this.state.page_index}</div>
+          <div>Total Count : {this.state.total}</div>
+          {/* <div>Page Index : {this.state.page_index}</div> */}
+        </div>
         <div className='table'>
-         <div className='table-row' id='table-header'>
-           <div className='table-cell'>수준</div>
-           <div className='table-cell'>詞彙</div>
-           <div className='table-cell'>어휘</div>
-           <div className='table-cell'>길잡이말</div>
-           <div className='table-cell'>품사</div>
-         </div>
-         {this.state.data_list.map((list, index) => (
-          <div className='table-row' id='table-body'>
-            <div className='table-cell'>{list.수준}</div>
-            <div className='table-cell'>{list.詞彙}</div>
-            <div className='table-cell'>{list.어휘}</div>
-            <div className='table-cell'>{list.길잡이말}</div>
-            <div className='table-cell'>{list.품사}</div>
+          <div className='table-row' id='table-header'>
+            <div className='table-cell'>수준</div>
+            <div className='table-cell'>詞彙</div>
+            <div className='table-cell'>어휘</div>
+            <div className='table-cell'>길잡이말</div>
+            <div className='table-cell'>품사</div>
           </div>
-    ))}
-    </div>
-    </div>
+          {this.state.data_list.map((list, index) => (
+            <div className='table-row' id='table-body'>
+              <div className='table-cell'>{list.수준}</div>
+              <div className='table-cell'>{list.詞彙}</div>
+              <div className='table-cell'>{list.어휘}</div>
+              <div className='table-cell'>{list.길잡이말}</div>
+              <div className='table-cell'>{list.품사}</div>
+            </div>
+          ))}
+        </div>
+      </div>
     )
   }
 }
 
-
-
-
-// class Topik2Vocabulary extends React.Component {
-
-//   state = {
-//     data: {
-//       data_list: []
-//     },
-//   }
-
-//   // 舊版用法
-//   // constructor(props){
-//   //   super(props)
-//   //   this.state = {
-//   //     data:{
-//   //       data_list:[]
-//   //     },
-//   //   }
-//   // }
-
-//   componentDidMount() {
-//     fetchTopik2Vocabulary().then(({ data }) => {
-//       this.setState({
-//         data
-//       })
-//     })
-//   }
-
-//   render() {
-//     const { data } = this.state;
-//     return (
-//       <div className='table'>
-//         <div className='table-row' id='table-header'>
-//           <div className='table-cell'>수준</div>
-//           <div className='table-cell'>詞彙</div>
-//           <div className='table-cell'>어휘</div>
-//           <div className='table-cell'>길잡이말</div>
-//           <div className='table-cell'>품사</div>
-//         </div>
-//         {data.data_list.map((data) =>
-//           <div className='table-row' id='table-body'>
-//             <div className='table-cell'>{data.수준}</div>
-//             <div className='table-cell'>{data.詞彙}</div>
-//             <div className='table-cell'>{data.어휘}</div>
-//             <div className='table-cell'>{data.길잡이말}</div>
-//             <div className='table-cell'>{data.품사}</div>
-//           </div>
-//         )}
-//       </div>
-//     )
-//   }
-// }
-
 ReactDOM.render(
   <React.StrictMode>
-    {/* <GetResponseInfo/> */}
-    {/* <Topik2Vocabulary /> */}
-    <TestOne />
-    {/* <Pagination /> */}
+    <Topik2Vocabulary />
   </React.StrictMode>,
   document.getElementById('root')
 );
